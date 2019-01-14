a87a882f08bc2f0f9a8f26aeedcb61b299a9bae8
Trent Willingham <TrentWillingham@gmail.com>
2019-01-14 11:34:20 -0500

    fix reboot logic and remove automatic scanning

---
+++ Scripts/UpdateDownloader.ps1
@@ -0,0 +1,29 @@
+﻿$UpdateSession = New-Object -ComObject 'Microsoft.Update.Session'
+$UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
+$SearchResult = $UpdateSearcher.Search("IsInstalled=0 and IsHidden=0")
+$DownloadCount = 0
+
+if ( $searchResult.Updates.Count -eq 0 ) {
+    return 0
+}
+
+$UpdatesToDownload = New-Object -ComObject "Microsoft.Update.UpdateColl"
+foreach ( $Update in $SearchResult.Updates ) {
+    if ( $Update.IsDownloaded -eq $false ) {
+        $UpdatesToDownload.Add( $Update ) | Out-Null
+    }
+}
+if ( $UpdatesToDownload.Count -gt 0 ) {
+    $Downloader = $UpdateSession.CreateUpdateDownloader()
+    $Downloader.Updates = $UpdatesToDownload
+    $DownloadResult = $Downloader.Download()
+
+
+    0..( $UpdatesToDownload.Count - 1 ) | ForEach-Object {
+        $Result = $DownloadResult.GetUpdateResult( $PSItem ).ResultCode
+        if ( $Result -eq 2 -or $Result -eq 3 ) {
+            $DownloadCount++
+        }
+    }
+}
+return $DownloadCount
